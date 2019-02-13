19200c24d4a52a9510a75d1f8f1ed02211a5eef1
Егор <31363936+EgorFrom@users.noreply.github.com>
2019-02-13 19:59:08 +0500

    Use Express

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-var fs = require('fs');
+var express = require('express');
 
-//connecting to local server
-var http = require('http');
+var app = express();
 
-var server = http.createServer(function(req, res) {
-	console.log("Page URL" + req.url);
-	if (req.url === '/index' || req.url === '/')
-	{
-		res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});//200 -page found, what date we get?
-		fs.createReadStream(__dirname + '/index.html').pipe(res);
-	} else if (req.url === '/about') {
-		res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});//200 -page found, what date we get?
-		fs.createReadStream(__dirname + '/about.html').pipe(res);
-	} else {
-		res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});//200 -page found, what date we get?
-		fs.createReadStream(__dirname + '/404.html').pipe(res);
-	}
+app.get('/', function (req, res) {
+	res.send('This is home');
 });
 
-server.listen(3000, '127.0.0.1');//port + ip
-console.log("We looking at prot 3000");
+app.get('/news', function (req, res) {
+	res.send('This is news');
+});
+
+app.get('/news/:id/:name', function (req, res) {
+	res.send('This is news. Id is - ' + req.params.id + req.params.name);
+});
+
+app.listen(3000);
+
+
