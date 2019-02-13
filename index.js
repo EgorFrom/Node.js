ce9625b75ad8f3c60efd9c95d85285dcbae93570
Егор <31363936+EgorFrom@users.noreply.github.com>
2019-02-13 18:33:26 +0500

    linked work with streams and creating server in one project. Loading html file.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
+
+//connecting to local server
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
 	console.log("Page URL" + req.url);
-	res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});//200 -page found, what date we get?
-	res.end('Hello, world!');	
+	res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});//200 -page found, what date we get?
+	var myReadShort = fs.createReadStream(__dirname + '/index.html', 'utf8');
+
+	myReadShort.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');//port + ip
