e489e80e185d633680a63b74b6337e779f71d6dc
Егор <31363936+EgorFrom@users.noreply.github.com>
2019-02-13 15:46:39 +0500

    Created/Deleted dir and link with files.

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-//ассинхронное
-// var fs = require('fs'); //file system
-
-
-// var file_reader = fs.readFile('text.txt', 'utf8', function(err, data) {
-//	функция сработает по окончанию
-//	выводим ошибку, если она есть
-//	data == file_reader
-// 	console.log(data);
-// });
-
-// var message = "Привет мир!";
-// fs.writeFile('outText.txt', message, function(err, data) {
-//	функция сработает по окончанию
-//	выводим ошибку, если она есть
-//	data == message
-// 	console.log(data);
-// });
-
 var fs = require('fs'); //file system
 
-var file_reader = fs.readFileSync('text.txt', 'utf8'); //пока не прочтем, дальше не пойдем
+// fs.unlink('outText.txt', function() {
+// 	//begin after delete
+// });//Async
 
-var message = "Привет мир!\n" + file_reader;
-fs.writeFileSync('outText.txt', message);//создаем/перезаписываем файл
+//Sync
+// fs.mkdirSync('new-one');//create
+// fs.mkdirSync('new-two');//create
+// fs.rmdirSync('new-one');//delete
+
+//Async create
+fs.mkdir('new-one', function(){
+	fs.writeFile('./new-one/some_new.txt', "Hello, world!", function(){
+		console.log('All ok');
+	});
+});
+
+// //Async delete
+// fs.unlink('./new-one/some_new.txt', function() {
+// 	fs.rmdir('new-one', function(){});
+// });
